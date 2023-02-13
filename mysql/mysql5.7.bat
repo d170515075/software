f21e4468174bef9ace59244bc23787d390438b62
xiangzi9626 <170515075@qq.com>
2023-02-13 13:56:22 +0800

    1.3

---
+++ mysql/mysql5.7.bat
@@ -0,0 +1,5 @@
+cd/
+D:
+cd software\phpstudy_pro\Extensions\MySQL5.7.26\bin
+mysql -uroot -proot
+start
---
+++ mysql/mysql备份.bat
@@ -0,0 +1,5 @@
+cd/
+D:
+cd software\phpstudy_pro\Extensions\MySQL5.7.26\bin
+mysqldump -uroot -proot laravel>D:\1.sql
+start
